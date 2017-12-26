1ba724c01c551e1f7044b852f813bb46bd1e95f8
Kamil Mróz <mroziqella@o2.pl>
2017-12-26 19:37:16 +0100

    added angular routing

--- backend/src/main/java/pl/szul/organizer/day/domain/DayRepository.java
+++ backend/src/main/java/pl/szul/organizer/day/domain/DayRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(exported = false)
-public interface DayRepository extends MongoRepository<DayDocument,String> {
+interface DayRepository extends MongoRepository<DayDocument,String> {
 }
